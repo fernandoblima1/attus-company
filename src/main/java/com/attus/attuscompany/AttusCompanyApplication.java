856698d317f6a57c2a7390bcb3cbeac4451dea89
Luis Fernando Borges de Lima <luis.fernando@etipi.pi.gov.br>
2024-05-08 14:57:58 -0300

    :construction: Initial commit

---
+++ src/main/java/com/attus/attuscompany/AttusCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.attus.attuscompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttusCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attus/attuscompany/AttusCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attus.attuscompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttusCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
